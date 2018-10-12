578727301dc216511e77cf8efd9ca85e2a3dd8c0
walidone <walidonehafiane@gmail.com>
2018-10-12 18:19:57 +0200

    controleur

--- src/Controleur/Controleur.java
+++ src/Controleur/Controleur.java
@@ -1,5 +1,50 @@
-package Controleur;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package acl2018.groupe5;
 
+import java.util.Scanner;
+
+/**
+ *
+ * @author walidone
+ */
 public class Controleur {
+    private Jeux jeux;
+    private int derection;
 
+    public Jeux getJeux() {
+        return jeux;
+    }
+
+    public void setJeux(Jeux jeux) {
+        this.jeux = jeux;
+    }
+     
+     void getDirection(){
+         Scanner s = new Scanner(System.in);
+         char c =s.nextLine().charAt(0);
+         if(c=="z"){
+          jeux.deplacement(Dynamique.NORD);  
+         }
+         if(c=="s"){
+          jeux.deplacement(Dynamique.SUD);
+         }
+         if(c=="d"){
+          jeux.deplacement(Dynamique.EST);
+         }
+         if(c=="q"){
+          jeux.deplacement(Dynamique.OUEST);
+         }
+         return 0;
+     }
+     
+     void start(){
+          while(true){
+        getDirection();
+        }
+     }
+     
 }
