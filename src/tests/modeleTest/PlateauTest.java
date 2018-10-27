68c769d8ae3ca9a106757ca15bd82b1ff7a6fbbb
AlaaBenk94 <a.benkarrad@gmail.com>
2018-10-27 15:42:02 +0200

    Test du constructeur Plateau.

--- src/tests/modeleTest/PlateauTest.java
+++ src/tests/modeleTest/PlateauTest.java
@@ -1,23 +1,34 @@
 package tests.modeleTest;
 
+import fr.ul.acl.modele.Plateau;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PlateauTest {
 
-    @org.junit.jupiter.api.BeforeEach
-    void setUp() {
-    }
-
-    @org.junit.jupiter.api.AfterEach
-    void tearDown() {
-    }
-
     @org.junit.jupiter.api.Test
-    void getHauteur() {
-    }
+    void Plateau(){
 
-    @org.junit.jupiter.api.Test
-    void getLargeur() {
+        // valeur positives
+        int l = 10, h=8;
+        Plateau p = new Plateau(l,h);
+        assertEquals(l,p.getLargeur());
+        assertEquals(h,p.getHauteur());
+
+        // valeur 0
+        l = 0; h=0;
+        try {
+            p = new Plateau(l, h);
+            fail("valeurs = 0 non gérées");
+        } catch (IllegalArgumentException e){}
+
+        // valeur negative
+        l = -5; h=0;
+        try {
+            p = new Plateau(l, h);
+            fail("valeurs négatives non gérées");
+        } catch (IllegalArgumentException e){}
+
     }
 
     @org.junit.jupiter.api.Test
