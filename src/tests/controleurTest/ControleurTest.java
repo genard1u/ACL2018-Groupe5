495f27452cad58bcdb7537761090664d72941a12
AlaaBenk94 <a.benkarrad@gmail.com>
2018-10-27 17:46:44 +0200

    Ajout test de constructur Controleur.

--- src/tests/controleurTest/ControleurTest.java
+++ src/tests/controleurTest/ControleurTest.java
@@ -1,23 +1,23 @@
 package tests.controleurTest;
 
+import fr.ul.acl.controleur.Controleur;
+import fr.ul.acl.modele.Jeu;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ControleurTest {
 
-    @org.junit.jupiter.api.BeforeEach
-    void setUp() {
-    }
-
-    @org.junit.jupiter.api.AfterEach
-    void tearDown() {
-    }
-
     @org.junit.jupiter.api.Test
-    void getJeu() {
-    }
+    void Controleur(){
+        Jeu j = new Jeu(10,10);
+        Controleur c = new Controleur(j);
+        assertSame(j,c.getJeu());
 
-    @org.junit.jupiter.api.Test
-    void setJeu() {
+        try {
+            c = new Controleur(null);
+            fail("valeur null non gérée.");
+        } catch (NullPointerException e) {}
+
     }
 
     @org.junit.jupiter.api.Test
