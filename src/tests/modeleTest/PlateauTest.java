51c37ccd2f3675782ea708ae814dea7384dc0d9c
AlaaBenk94 <a.benkarrad@gmail.com>
2018-10-27 16:38:41 +0200

    Ajout des test de la class Plateau.

--- src/tests/modeleTest/PlateauTest.java
+++ src/tests/modeleTest/PlateauTest.java
@@ -2,6 +2,8 @@ package tests.modeleTest;
 
 import fr.ul.acl.modele.Plateau;
 
+import java.util.Random;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PlateauTest {
@@ -33,17 +35,38 @@ class PlateauTest {
 
     @org.junit.jupiter.api.Test
     void allerNord() {
+        int l = 10, h=10;
+        Plateau p = new Plateau(l,h);
+        assertTrue(p.allerNord((int) Math.random()%l,1));
+        assertFalse(p.allerNord((int) Math.random()%l,0));
+
+        // Test d'erreur non effectué.
     }
 
     @org.junit.jupiter.api.Test
     void allerSud() {
+        int l = 10, h=10;
+        Plateau p = new Plateau(l,h);
+        assertTrue(p.allerSud((int) Math.random()%l,5));
+        assertFalse(p.allerSud((int) Math.random()%l,h-1));
+        // Test d'erreur non effectué.
     }
 
     @org.junit.jupiter.api.Test
     void allerEst() {
+        int l = 10, h=10;
+        Plateau p = new Plateau(l,h);
+        assertTrue(p.allerEst(5,(int) Math.random()%h));
+        assertFalse(p.allerEst(l-1,(int) Math.random()%h));
+        // Test d'erreur non effectué.
     }
 
     @org.junit.jupiter.api.Test
     void allerOuest() {
+        int l = 10, h=10;
+        Plateau p = new Plateau(l,h);
+        assertTrue(p.allerOuest(5,(int) Math.random()%h));
+        assertFalse(p.allerOuest(0,(int) Math.random()%h));
+        // Test d'erreur non effectué.
     }
 }
