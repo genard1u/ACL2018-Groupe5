8ffa5330ffcc972e050f63f8edaf25e1c2caa4a1
AlaaBenk94 <a.benkarrad@gmail.com>
2018-11-16 15:45:22 +0100

    Ajout de la case Trésor (Treasure) et Invincible.

--- src/fr/ul/acl/model/Treasure.java
+++ src/fr/ul/acl/model/Treasure.java
@@ -1,4 +1,31 @@
 package fr.ul.acl.model;
 
+/**
+ * la classe des case trésor, si le héros arrive sur la case il a gagné le jeu!.
+ */
 public class Treasure extends Magic {
+
+    public static final String TREASURE = "TREASURE";
+
+    /**
+     * Constructeur
+     * @param posX la position x
+     * @param posY la position y
+     */
+    public Treasure(int posX, int posY) {
+        super(posX, posY, TREASURE);
+    }
+
+
+
+    @Deprecated
+    @Override
+    public String getAffichage() {
+        return "$";
+    }
+
+    @Override
+    public void effet(Heros h) {
+        h.Wins();
+    }
 }
