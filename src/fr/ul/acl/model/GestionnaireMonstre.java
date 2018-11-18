c3971e65102bfd809d29080535f3663c0abffd3b
AlaaBenk94 <a.benkarrad@gmail.com>
2018-11-18 04:22:21 +0100

    Checkout missing files from walid's branch.

---
+++ src/fr/ul/acl/model/GestionnaireMonstre.java
@@ -0,0 +1,96 @@
+package fr.ul.acl.model;
+
+import fr.ul.acl.engine.Cmd;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public abstract class GestionnaireMonstre  {
+    protected ArrayList<AbstractMonstre> monstres;
+    protected Plateau plateau;
+    protected Jeu jeu;
+    /**
+     *  placer nbmonstres monstres et nbfontome fontomes
+     * @param nbmonstres nombre de monstre
+     * @param nbfontome  nombre de fontome
+     * @param jeu jeu ou se trouve les monstre
+     */
+    public GestionnaireMonstre(int nbmonstres,int nbfontome,Jeu jeu){
+        if(nbfontome<0||nbfontome<0||jeu==null)throw new IllegalArgumentException();
+        this.jeu=jeu;
+        this.plateau=jeu.getPlateau();
+        monstres=new ArrayList<AbstractMonstre>();
+        int posx,posy;
+        Random rand =new Random();
+        for (int i=0;i<nbmonstres;i++) {
+            do {
+                posx = rand.nextInt(plateau.getLargeur());
+                posy = rand.nextInt(plateau.getHauteur());
+            } while (plateau.getTypeCase(posx, posy) != "");
+            monstres.add(new Monstre(posx,posy));
+        }
+        for (int i=0;i<nbfontome;i++) {
+            do {
+                posx = rand.nextInt(plateau.getLargeur());
+                posy = rand.nextInt(plateau.getHauteur());
+            } while (plateau.getTypeCase(posx, posy) != "");
+            monstres.add(new Fantome(posx,posy));
+        }
+
+    }
+
+    /**
+     *  deplacer les monstre et les fontome */
+    public abstract void deplacement();
+
+    /**
+     *recuperer les position des monstres
+     *@return lise des position des monstres
+     */
+    public ArrayList<int[]>getPosMonstres(){
+        ArrayList<int[]> pos=new ArrayList<int[]>();
+        for (AbstractMonstre m :monstres){
+            int type=1;
+            if(m.getType().equals(Monstre.MONSTRE))type=0;
+            pos.add(new int[]{m.posX,m.posY,type});
+        }
+        return pos;
+    }
+
+    /**
+     * verification si la case (x,y) est une case de monstre
+     * @param x position x a verifier
+     * @param y position y a verifier
+     * @return true si il y a un monstre dans la position x , y
+     */
+    public boolean isMonstre(int x, int y){
+        for (AbstractMonstre m :monstres){
+            if(m.getPosX()==x&&m.getPosY()==y)return true;
+        }
+        return false;
+    }
+
+    /**
+     * recuperer touts les monstres
+     * @return liste des monstre
+     */
+    public ArrayList<AbstractMonstre> getMonstres() {
+        return monstres;
+    }
+
+    /**
+     * recuperer le jeu
+     * @return jeu
+     */
+    public Jeu getJeu() {
+        return jeu;
+    }
+
+    /**
+     * recuperer le plateux
+     * @return le plateux
+     */
+    public Plateau getPlateau() {
+        return plateau;
+    }
+}
---
+++ src/tests/modeleTest/TrapTest.java
@@ -0,0 +1,65 @@
+package tests.modeleTest;
+
+import fr.ul.acl.Resources;
+import fr.ul.acl.model.Heros;
+import fr.ul.acl.model.Trap;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TrapTest {
+    @Test
+    public void effetRhigt() {
+        Heros heros =new Heros(3,2);
+        Trap trap =new Trap(3,2);
+        trap.effet(heros);
+        assertTrue(heros.isDead());
+    }
+    /**
+     * teste constructeur , posision x negative
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void negativePosX(){
+        Trap trap =new Trap(-3,2);
+    }
+    /**
+     * teste constructeur , posision y negative
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void negativePosY(){
+        Trap trap =new Trap(3,-2);
+    }
+    /**
+     * teste constructeur , posision x depase le largeur de plateau
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void limitPosX(){
+        Trap trap =new Trap(Resources.WIDTH,2);
+    }
+    /**
+     * teste constructeur , posision y depase le hauteur de plateau
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void limitPosY(){
+        Trap trap =new Trap(3,Resources.HEIGHT);
+    }
+    /**
+     * teste effet avec null
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void herosNull(){
+        Trap trap =new Trap(3,2);
+        trap.effet(null);
+    }
+    /**
+     * teste effet avec un heros qui n'est pas sur la case
+     */
+    @Test
+    public void positionHeros(){
+        Heros heros=new Heros(2,2);
+        heros.setInvincible(false);
+        Trap trap =new Trap(3,2);
+        trap.effet(heros);
+        assertFalse(heros.isDead());
+    }
+}
