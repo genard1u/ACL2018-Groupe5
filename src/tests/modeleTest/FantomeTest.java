201388d6874029fefcd576d89efbd482fc7875ef
AlaaBenk94 <a.benkarrad@gmail.com>
2018-11-19 01:56:40 +0100

    Mise en oeuvre des tests des classes Fantome & Monstre.

--- src/tests/modeleTest/FantomeTest.java
+++ src/tests/modeleTest/FantomeTest.java
@@ -46,15 +46,35 @@ public class FantomeTest {
     }
 
     /**
+     * Cette methode génère les parametres pour le test de constructeur.
+     * @return Les parametres Min de la methode.
+     */
+    public static Object[] getMinBound() {
+        return $(   $(Resources.WIDTH-(Resources.WIDTH/2),-5),
+                $(-5,Resources.HEIGHT-(Resources.HEIGHT/2)),
+                $(-5,-5)    );
+    }
+
+    /**
      * Test de Constructeur.
      * Boundary.
      * Minimum
      */
-    @Test(expected = AssertionError.class)
-    public void FantomeOutOfBoundMin(){
-        int x=0,y=-1;
+    @Test(expected = IllegalArgumentException.class)
+    @Parameters(method = "getMinBound")
+    public void FantomeOutOfBoundMin(int x, int y){
         Fantome f = getFantome(x,y);
         fail("Out Of Bound Not handled Min");
+    }
+
+    /**
+     * Cette methode génère les parametres pour le test de constructeur.
+     * @return Les parametres Max de la methode.
+     */
+    public static Object[] getMaxBound() {
+        return $(   $(Resources.WIDTH-(Resources.WIDTH/2),Resources.HEIGHT+5),
+                $(Resources.WIDTH+5,Resources.HEIGHT-(Resources.HEIGHT/2)),
+                $(Resources.WIDTH+5,Resources.HEIGHT+5) );
     }
 
     /**
@@ -62,9 +82,9 @@ public class FantomeTest {
      * Boundary.
      * Maximum
      */
-    @Test(expected = AssertionError.class)
-    public void FantomeOutOfBoundMax(){
-        int x= Resources.WIDTH+3,y=Resources.HEIGHT+2;
+    @Test(expected = IllegalArgumentException.class)
+    @Parameters(method = "getMaxBound")
+    public void FantomeOutOfBoundMax(int x, int y){
         Fantome f = getFantome(x,y);
         fail("Out Of Bound Not handled Max");
     }
@@ -127,7 +147,7 @@ public class FantomeTest {
     }
 
     /**
-     * Method Factory de la class Monstre.
+     * Method Factory de la class Fantome.
      * @param x position x
      * @param y position y
      * @return une instance de Fantome.
