0a220fcd57e6147887e0ad21c7aa0f9aea07e9bf
evidlream <yanncael@gmail.com>
2018-11-28 19:25:55 +0100

    Remise des tests

---
+++ src/tests/modeleTest/GestionnaireMonstreIntelligentsTest.java
@@ -0,0 +1,171 @@
+package tests.modeleTest;
+
+import fr.ul.acl.Resources;
+import fr.ul.acl.engine.Cmd;
+import fr.ul.acl.model.*;
+import fr.ul.acl.model.monstre.Algorithme;
+import fr.ul.acl.model.monstre.GestionnaireMonstreIntelligents;
+import fr.ul.acl.model.monstre.GestionnaireMonstre;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.LinkedList;
+
+import static junitparams.JUnitParamsRunner.$;
+import static org.junit.Assert.*;
+
+/**
+ * Test de la classe GestionnaireMonstreIntelligents.
+ * Elements de test : Constructeur.
+ */
+@RunWith(JUnitParamsRunner.class)
+public class GestionnaireMonstreIntelligentsTest {
+
+    Jeu j;
+    Algorithme algo;
+
+    /**
+     * Setup avant l'execution de test.
+     */
+    @Before
+    public void setUp(){
+        j = JeuImitation.getInstance();
+        algo = AlgoImitation.getInstance();
+    }
+
+    /**
+     * Test de Constructeur.
+     * Right.
+     * Same Jeu
+     */
+    @Test
+    public void GestionnaireMonstreIntellSameJeu(){
+        int nm=5,nf=5;
+        GestionnaireMonstre gma = getGestionnaire(nm, nf, j, algo);
+        assertSame("Le meme Jeu",j,gma.getJeu());
+    }
+
+    /**
+     * Test de Constructeur.
+     * Right.
+     * Nombre Fantome
+     */
+    @Test
+    public void GestionnaireMonstreIntellEqualNbMonstres(){
+        int nm=5,nf=5;
+        GestionnaireMonstre gma = getGestionnaire(nm, nf, j, algo);
+        assertEquals("Le nombre de monstre est le meme",nm+nf,gma.getMonstres().size());
+    }
+
+    /**
+     * Cette methode génère les parametres pour le test de constructeur.
+     * @return Les parametres Min de la methode.
+     */
+    public static Object[] getNbMonstres() {
+        return $(   $(Resources.HEIGHT*Resources.WIDTH+1,0),
+                $(0,Resources.HEIGHT*Resources.WIDTH+1),
+                $(-5,-5),
+                $(0,-5),
+                $(-5,0)    );
+    }
+
+    /**
+     * Test de Constructeur.
+     * Boundary.
+     * Nombre Fantome et Monstres.
+     */
+    @Test(expected = IllegalArgumentException.class)
+    @Parameters(method = "getNbMonstres")
+    public void GestionnaireMonstreIntellNbMonstres(int nm, int nf){
+        GestionnaireMonstre gma = getGestionnaire(nm, nf, j, algo);
+        fail("Boundaries Exceeded");
+    }
+
+    /**
+     * Test Constructeur.
+     * Right.
+     * Chemin NotNull.
+     */
+    @Test
+    public void GestionnaireMonstreIntellCheminNotNull(){
+        int nm=5,nf=5;
+        GestionnaireMonstre gma = getGestionnaire(nm, nf, j, algo);
+        assertNotNull("Le chemin est null.",((GestionnaireMonstreIntelligents) gma).getChemins());
+    }
+
+    /**
+     * Test Constructeur.
+     * Right.
+     * Chemin construit.
+     */
+    @Test
+    public void GestionnaireMonstreIntellCheminConstruit(){
+        int nm=1,nf=0;
+        GestionnaireMonstre gma = getGestionnaire(nm, nf, j, algo);
+        assertEquals("Le chemin n'est pas bien construit.",
+                algo.getChemin(null,0,0,0,0,null),
+                ((GestionnaireMonstreIntelligents) gma).getChemins().get(0));
+    }
+
+
+    /**
+     * method Factory de gestionnaireMonstreIntelligents.
+     * @param nm nombre de monstres
+     * @param nf nombre de fantomes
+     * @param j le jeu
+     * @return une instance de gestionnaireMonstreAleatoire.
+     */
+    public static GestionnaireMonstreIntelligents getGestionnaire(int nm, int nf, Jeu j, Algorithme algo){
+        return new GestionnaireMonstreIntelligents(nm, nf, j, algo);
+    }
+}
+
+/**
+ * Imitation de la classe Jeu ( Pour les tests unitaires ).
+ */
+class JeuImitation extends Jeu{
+
+    public JeuImitation(int largeur, int hauteur) {
+        super(largeur, hauteur);
+    }
+
+    @Override
+    public int herosPosX() {
+        return 5;
+    }
+
+    @Override
+    public int herosPosY() {
+        return 5;
+    }
+
+    public static JeuImitation getInstance() {
+        return new JeuImitation(10, 10);
+    }
+}
+
+
+
+
+/**
+ * Imitation de la classe Algorithme ( Pour les tests unitaires ).
+ */
+class AlgoImitation implements Algorithme {
+
+    public static Algorithme getInstance() {
+        return new AlgoImitation();
+    }
+
+    @Override
+    public LinkedList<Cmd> getChemin(Plateau plateau, int Hx, int Hy, int x, int y, String type) {
+        LinkedList l = new LinkedList<>();
+        l.add(Cmd.UP);
+        l.add(Cmd.DOWN);
+        l.add(Cmd.RIGHT);
+        l.add(Cmd.LEFT);
+        return l;
+    }
+}
---
+++ src/tests/modeleTest/PlateauTest.java
@@ -0,0 +1,197 @@
+package tests.modeleTest;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.Ignore;
+
+import fr.ul.acl.model.Plateau;
+
+
+/**
+ * Tests de la classe Plateau.
+ * Elements de test : Constructeur, isAccessible (les autres methodes sont considérées comme "Too Small To Fail").
+ */
+public class PlateauTest {
+
+    /**
+     * Test de Constructeur.
+     * Right.
+     * Dimensions largeur.
+     */
+    @Test
+    public void PlateauDimLargeur(){
+        int l=10,h=10;
+        Plateau p = getPlateau(l,h);
+        assertEquals(l,p.getLargeur());
+    }
+
+    /**
+     * Test de Constructeur.
+     * Right.
+     * Dimensions hauteur.
+     */
+    @Test
+    public void PlateauDimHauteur(){
+        int l=10,h=10;
+        Plateau p = getPlateau(l,h);
+        assertEquals(h,p.getHauteur());
+    }
+
+    /**
+     * Test de Constructeur.
+     * Right.
+     * Matrice.
+     */
+    @Ignore
+    @Test
+    public void PlateauMatrix(){
+        fail("Not Implemented Yet : encore pas de solution sans boucle.");
+    }
+
+    /**
+     * Test de Constructeur.
+     * Boundary.
+     * Zero Hauteur.
+     */
+    @Test(expected = AssertionError.class)
+    public void PlateauZeroHauteur(){
+        int l=5,h=0;
+        Plateau p = getPlateau(l,h);
+        fail("valeur 0 non gérée.");
+    }
+
+    /**
+     * Test de Constructeur.
+     * Boundary.
+     * Zero Largeur.
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void PlateauZeroLargeur(){
+        int l=0,h=5;
+        Plateau p = getPlateau(l,h);
+        fail("valeur 0 non gérée.");
+    }
+
+    /**
+     * Test de Constructeur.
+     * Boundary.
+     * Zero.
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void PlateauZero(){
+        int l=0,h=0;
+        Plateau p = getPlateau(l,h);
+        fail("valeur 0 non gérée.");
+    }
+
+    /**
+     * Test de Constructeur.
+     * Boundary.
+     * Negative -un seul: hauteur-.
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void PlateauNegativeHauteur(){
+        int l=4,h=-4;
+        Plateau p = getPlateau(l,h);
+        fail("valeurs negatives non gérées.");
+    }
+
+    /**
+     * Test de Constructeur.
+     * Boundary.
+     * Negative -un seul: largeur-.
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void PlateauNegativeLargeur(){
+        int l=-4,h=4;
+        Plateau p = getPlateau(l,h);
+        fail("valeurs negatives non gérées.");
+    }
+
+    /**
+     * Test de Constructeur.
+     * Boundary.
+     * Negative.
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void PlateauNegative(){
+        int l=-4,h=-4;
+        Plateau p = getPlateau(l,h);
+        fail("valeurs negatives non gérées.");
+    }
+
+    /**
+     * Test de Constructeur.
+     * Boundary.
+     * Dimensions non carrée.
+     */
+    @Test
+    public void PlateauNotSquareLargeur() throws ArrayIndexOutOfBoundsException{
+        int l=10,h=6;
+        Plateau p = getPlateau(l,h);
+        assertEquals(l,p.getLargeur());
+        assertEquals(h,p.getHauteur());
+    }
+
+    /**
+     * Test de Constructeur.
+     * Boundary.
+     * Dimensions non carrée.
+     */
+    @Test
+    public void PlateauNotSquareHauteur() throws ArrayIndexOutOfBoundsException{
+        int l=6,h=10;
+        Plateau p = getPlateau(l,h);
+        assertEquals(l,p.getLargeur());
+        assertEquals(h,p.getHauteur());
+    }
+
+    /**
+     * Test de la methode isAccessible
+     * Right.
+     * Affirmative.
+     */
+    @Test
+    public void isAccessibleAffirmative(){
+        int l=10,h=10;
+        Plateau p = getPlateau(l,h);
+        int[] coord = p.getPositionVide();
+        assertTrue(p.isAccessible(coord[0],coord[1]));
+    }
+
+    /**
+     * Test de la methode isAccessible
+     * Right.
+     * Negative.
+     */
+    @Test
+    public void isAccessibleNegative(){
+        int l=10,h=10;
+        int x=0,y=0;
+        Plateau p = getPlateau(l,h);
+        assertFalse(p.isAccessible(x,y));
+    }
+
+    /**
+     * Test de la methode isAccessible
+     * Boundary.
+     * Hors dimensions.
+     */
+    @Test(expected = ArrayIndexOutOfBoundsException.class)
+    public void isAccessibleOutOfBound(){
+        int l=10,h=10;
+        int x=15,y=0;
+        Plateau p = getPlateau(l,h);
+        assertTrue(p.isAccessible(x,y));
+        fail("valeurs Hors dimensions no gérées.");
+    }
+
+    /**
+     * method factory pour plateau.
+     * @param l largeur
+     * @param h heuteur
+     */
+    public static Plateau getPlateau(int l,int h){
+        return new Plateau(l,h);
+    }
+}
